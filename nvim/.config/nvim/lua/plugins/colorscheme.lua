9d3a9f65c9bbdb726e45fa5dedc4f7c76a602fdf
boning <boningmaple@gmail.com>
2026-06-07 14:39:10 +1000

    use mac-clear colorscheme in Neovim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,6 @@
 vim.pack.add({ "https://github.com/nvim-tree/nvim-web-devicons" })
--- vim.pack.add({ "https://github.com/boningmaple/mac-clear" })
+vim.pack.add({ "https://github.com/boningmaple/mac-clear" })
 
 require("nvim-web-devicons").setup()
-vim.cmd.packadd("mac-clear")
 
 vim.cmd.colorscheme("mac-clear")
