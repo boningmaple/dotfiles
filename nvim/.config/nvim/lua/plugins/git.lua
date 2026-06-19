71c3039e1bf7745406055f80bfd3d2a6b3de8b6f
boning <boningmaple@gmail.com>
2026-06-19 20:44:57 +1000

    nvim: update plugins

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,3 +1,9 @@
 vim.pack.add({ "https://github.com/lewis6991/gitsigns.nvim" })
+vim.pack.add({ "https://github.com/neogitorg/neogit" })
+-- vim.pack.add({ "https://github.com/esmuellert/codediff.nvim" })
 
 require("gitsigns").setup({ numhl = true })
+require("neogit").setup({
+    -- Use Treesitter to apply syntax highlighting to diff hunks
+    treesitter_diff_highlight = true,
+})
